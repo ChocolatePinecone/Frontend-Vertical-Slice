5112c2f184120ad9c146be087cbbbc12f116c78f
Jelmer Pijnappel <chocolatepinecone@gmail.com>
2021-12-27 23:37:25 +0100

    Updated gitignore and updated app to accomodate menu items better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Slice';
+  menuItems: MenuItem[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Call to API is placed here
+  }
+}
+
+interface MenuItem {
+  dish: string,
+  description: string,
+  price: number,
 }
