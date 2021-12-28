89c7689284d62ebfe966c88a102fde35a9945ce3
Jelmer Pijnappel <chocolatepinecone@gmail.com>
2021-12-28 14:22:19 +0100

    Added menu item component and linked it to menu items gotten from server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,14 @@ export class AppComponent implements OnInit {
   title = 'Slice';
   menuItems: MenuItem[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     // Call to API is placed here
+    this.http.get<MenuItemsResponse>("http://localhost:3001/menu-items", {observe: "body", responseType: "json"}).subscribe((resp: MenuItemsResponse) => {
+      this.menuItems = resp.menuItems;
+      console.log(resp);
+    })
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {MenuItemComponent} from "./menu-item/menu-item.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuItemComponent,
   ],
   imports: [
     BrowserModule,
